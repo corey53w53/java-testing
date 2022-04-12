a9e9d2e1acdea319acd8a5e2d2c626e32dd70258
Corey Wang <corefire2004@gmail.com>
2022-04-12 17:51:17 -0400

    init

---
+++ Test.java
@@ -0,0 +1,18 @@
+class test{
+    public static void main(String[] args){
+        int x=7;
+        for (int i = 0; i <= 10; i += 1) {
+            System.out.println("Corey got " + i + " bitches");
+        }
+        boolean bool = x==7;
+        if (bool){
+            System.out.println("Ran");
+        }
+        
+        int[] nums ={1,2,3,4};
+        for (int i=0;i<=3;i+=1){
+            System.out.println(nums[i]);
+        }
+
+    }
+}
