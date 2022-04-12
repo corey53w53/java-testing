21a889dddb173aeb42cb973f9196fd85bc608cda
Corey Wang <corefire2004@gmail.com>
2022-04-12 18:14:38 -0400

    strings, chars, and vowels

--- Test.java
+++ Test.java
@@ -1,18 +1,13 @@
 class test{
     public static void main(String[] args){
-        int[] nums ={1,2,3,4};
-        int n = nums.length;
-        int[] newnums = new int[n+1];
-        newnums[n] = 7;
-
-        for (int i=0;i<nums.length;i+=1){
-            newnums[i]=nums[i];
-            System.out.print(nums[i]+" ");
-        }
-        System.out.print("\n");
-
-        for (int i=0;i<newnums.length;i+=1){
-            System.out.print(newnums[i]+" ");
+        String s = "hello world";
+        String vowel_list="aeiou";
+        char[] s_list=s.toCharArray();
+        for (int i=0; i<s_list.length; i++){
+            String char_as_s=Character.toString(s_list[i]);
+            if (vowel_list.contains(char_as_s)){
+                System.out.println(char_as_s);
+            }
         }
     }
 }
