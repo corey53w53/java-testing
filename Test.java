266a64e72f6ed186e92b9a8b43c5fb946c3d942d
Corey Wang <corefire2004@gmail.com>
2022-04-12 18:03:51 -0400

    bools

--- Test.java
+++ Test.java
@@ -1,18 +1,23 @@
 class test{
     public static void main(String[] args){
-        int x=7;
-        for (int i = 0; i <= 10; i += 1) {
-            System.out.println("Corey got " + i + " bitches");
-        }
-        boolean bool = x==7;
+        boolean bool = (Integer.parseInt("7"))==7;
         if (bool){
             System.out.println("Ran");
         }
         
         int[] nums ={1,2,3,4};
-        for (int i=0;i<=3;i+=1){
-            System.out.println(nums[i]);
-        }
+        int n = nums.length;
+        int[] newnums = new int[n+1];
+        newnums[n] = 7;
 
+        for (int i=0;i<nums.length;i+=1){
+            System.out.print(nums[i]+" ");
+        }
+        System.out.print("\n");
+
+        for (int i=0;i<newnums.length;i+=1){
+            System.out.print(newnums[i]+" ");
+        }
     }
 }
+
