acd4f020eba43b9012d779ebb86c8415b7a6bda6
Corey Wang <corefire2004@gmail.com>
2022-04-12 18:19:38 -0400

    sums AND static vs nothing

--- Test.java
+++ Test.java
@@ -1,11 +1,24 @@
+import javax.print.attribute.standard.MediaSize.Other;
+
 class test{
-    public static void main(String[] args){
-        int[] my_arr = {1,2,3,4};
+
+    public static int myfunc(int[] arr){
         int sum=0;
-        for (int i=0;i<my_arr.length;i++){
-            sum+=my_arr[i];
+        for (int i=0;i<arr.length;i++){
+            sum+=arr[i];
         }
-        System.out.println(sum);
+        return sum;
+    }
+    public static void main(String[] args){
+        OtherClass oc = new OtherClass();
+        int[] my_arr = {1,2,3,4};
+        System.out.println(myfunc(my_arr));
+        System.out.println(oc.test());
     }
 }
 
+class OtherClass {
+    public int test() {
+        return 1;
+    }
+}
