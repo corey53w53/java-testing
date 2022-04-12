b9e13a9b26afcded6fb27013e01056e8fb7d6310
Corey Wang <corefire2004@gmail.com>
2022-04-12 18:05:00 -0400

    appending to array

--- Test.java
+++ Test.java
@@ -1,16 +1,12 @@
 class test{
     public static void main(String[] args){
-        boolean bool = (Integer.parseInt("7"))==7;
-        if (bool){
-            System.out.println("Ran");
-        }
-        
         int[] nums ={1,2,3,4};
         int n = nums.length;
         int[] newnums = new int[n+1];
         newnums[n] = 7;
 
         for (int i=0;i<nums.length;i+=1){
+            newnums[i]=nums[i];
             System.out.print(nums[i]+" ");
         }
         System.out.print("\n");
