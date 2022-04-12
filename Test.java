5d8434f33df1af7410ee4d9a195a219436084fe2
Corey Wang <corefire2004@gmail.com>
2022-04-12 18:16:12 -0400

    array sums

--- Test.java
+++ Test.java
@@ -1,14 +1,11 @@
 class test{
     public static void main(String[] args){
-        String s = "hello world";
-        String vowel_list="aeiou";
-        char[] s_list=s.toCharArray();
-        for (int i=0; i<s_list.length; i++){
-            String char_as_s=Character.toString(s_list[i]);
-            if (vowel_list.contains(char_as_s)){
-                System.out.println(char_as_s);
-            }
+        int[] my_arr = {1,2,3,4};
+        int sum=0;
+        for (int i=0;i<my_arr.length;i++){
+            sum+=my_arr[i];
         }
+        System.out.println(sum);
     }
 }
 
