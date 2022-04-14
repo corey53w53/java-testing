00d35c2e83458f3e9d63db23bc86ccd1d8c99b2d
Corey Wang <corefire2004@gmail.com>
2022-04-13 20:38:04 -0400

    new name

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
-public class Main {
+public class test {
     int x = 5;
-
     public static void main(String[] args) {
-        Main myObj = new Main();
+        test myObj = new test();
         System.out.println(myObj.x);
     }
 }
