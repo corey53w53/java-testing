fe40587f1a1100320c8e18018431ab0bd4ba79ad
Corey Wang <corefire2004@gmail.com>
2022-04-13 20:46:39 -0400

    string method and inc age

--- Test.java
+++ Test.java
@@ -3,5 +3,25 @@ public class test {
     public static void main(String[] args) {
         test myObj = new test();
         System.out.println(myObj.x);
+        Person havish = new Person();
+        havish.incrementAge();
+        System.out.println(havish.age);
+        System.out.println(havish);
+
+    }
+}
+
+class Person {
+    int age =15;
+    public Person(){
+        System.out.println("I am " + age + " years old.");
+    }
+    public void incrementAge(){
+        this.age+=1;
+    }
+
+    @Override
+    public String toString(){
+        return "I am " + this.age + " years old";
     }
 }
