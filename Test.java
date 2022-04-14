d948b0d91eb5753f138716f2a8ab78ea80acb123
Corey Wang <corefire2004@gmail.com>
2022-04-13 21:08:45 -0400

    add method

--- Test.java
+++ Test.java
@@ -1,16 +1,11 @@
 public class test {
-    int x = 5;
     public static void main(String[] args) {
-        test myObj = new test();
-        System.out.println(myObj.x);
-
         Point p1 = new Point(-3,0);
-        System.out.println(p1);
-        p1.moveToOrigin();
-        System.out.println(p1);
+        Point p2 = new Point(0,2);
+        Point p3 = p1.add(p2);
+        System.out.println(p3);
     }
 }
-
 class Point {
     int x;
     int y;
@@ -31,13 +26,16 @@ class Point {
         } else if (this.y>0){
             this.y-=1;
         }
-
+    }
+    public Point add(Point p2){
+        int new_x = this.x+p2.x;
+        int new_y = this.y+p2.y;
+        return new Point(new_x,new_y);
     }
 
     @Override
     public String toString(){
         return "("+x+","+y+")";
     }
-}
 
-// ================================
+}
