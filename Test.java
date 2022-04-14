6d9770493adc71379dbc37998afe6e96e5e84ece
Corey Wang <corefire2004@gmail.com>
2022-04-13 20:35:20 -0400

    attributes

--- Test.java
+++ Test.java
@@ -1,24 +1,8 @@
-import javax.print.attribute.standard.MediaSize.Other;
+public class Main {
+    int x = 5;
 
-class test{
-
-    public static int myfunc(int[] arr){
-        int sum=0;
-        for (int i=0;i<arr.length;i++){
-            sum+=arr[i];
-        }
-        return sum;
-    }
-    public static void main(String[] args){
-        OtherClass oc = new OtherClass();
-        int[] my_arr = {1,2,3,4};
-        System.out.println(myfunc(my_arr));
-        System.out.println(oc.test());
-    }
-}
-
-class OtherClass {
-    public int test() {
-        return 1;
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj.x);
     }
 }
